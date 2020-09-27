d701f14cca79e53436af6580ad3dd08212fe6a21
natetheadequate <natewhite345@gmail.com>
2020-09-26 23:43:32 -0400

    can get song bounds

--- getBounds.js
+++ getBounds.js
@@ -1,21 +1,22 @@
-const { fstat } = require("fs");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const fs = require("fs");
-const song = process.argv[3];
+const { exec } = require("child_process");
+const file = fs.readFileSync('CurrentlyDisplayed.xml').toString();
+let song = process.argv[3];
 const artist = process.argv[2];
-const dom = new JSDOM(``, {
-    url: "file:///home/Documents/Programs/Trebel-transfer/CurrentlyDisplayed.xml",
-    contentType: "text/xml",
-    storageQuota: 10000000
+const dom = new JSDOM(file, {
+    contentType: 'text/xml'
 });
-el = (dom.window.document.querySelector('node[resource-id="com.mmm.trebelmusic:id/recyclerView"] node[text*="' + song + '"]~node[resource-id="com.mmm.trebelmusic:id/explicity"]~node[text="' + artist + '"]'))
+const el = (dom.window.document.querySelector('node[resource-id="com.mmm.trebelmusic:id/recyclerView"] node[text*=\"' + song + '\"]~node[resource-id="com.mmm.trebelmusic:id/explicity"]~node[text="' + artist + '"]'))
     || (dom.window.document.querySelector('node[resource-id="com.mmm.trebelmusic:id/recyclerView"] node[text*="' + song + '"]~node[text="' + artist + '"]'))
 if (el) {
-    boundsarr = el.getAttribute('bounds').replace('][', ',').toArray();
-    console.log((boundsarr[0] + boundsarr[1]) / 2 + ' ' + (boundsarr[3] + boundsarr[4]) / 2)
+    let boundsarr = el.getAttribute('bounds').replace('][', ',').replace('[', '').replace(']', '').split(',');
+    const x = (+boundsarr[0] + +boundsarr[1]) / 2;
+    const y = (+boundsarr[2] + +boundsarr[3]) / 2;
+    exec('adb shell input tap '+x+' '+y+';sleep 2;adb shell input tap 600 750; #Download for free');
 } else {
-    fs.writeFile(__dirname+'/errorLog.txt', {artist, song});
-    console.log('Cant find song');
+    fs.writeFile(__dirname + '/errorLog.txt', artist + ' ' + song + '\n', () => { });
+    exec("echo '"+song+" by "+artist+"' >>errorLog.txt;adb shell input tap 908 322 # clear search bar;");
 }
-
+///getbounds needs to be separate function ads move the page so we need to be able to get the position dynamically
